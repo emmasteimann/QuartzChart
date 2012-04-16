1cf14208c968d2a2a18cb0e37eb9e8396fc6d8ff
Emma Steimann <emmasteimann@gmail.com>
2012-04-16 11:11:20 -0500

    Initial Commit

---
+++ testQuartz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testQuartz
+//
+//  Created by Emma Steimann on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ testQuartz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testQuartz
+//
+//  Created by Emma Steimann on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
